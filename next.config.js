f4866f67e235ec8936473afcb2dfef1b255015ed
kanwaljitsingh <kanwaljitsingh@cssoftsolutions.com>
2025-01-30 19:11:52 +0530

    config setup

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  experimental: {
+    taint: true,
+  },
+  reactStrictMode: true,
+  generateOffline: true,
+  env: {
+    API_BASE_URL: "http://localhost:3000/api",
+  },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,23 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#1369D9",
+        border: "#E0E8F2",
+        background: "#F5F7FA",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: "#111827",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
